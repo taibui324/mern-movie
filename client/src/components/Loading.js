635b0c5048e73a578598ab8384d803c3e279d265
taibui324 <taibui324@gmail.com>
2021-04-16 21:45:53 +0700

    commit

---
+++ client/src/components/Loading.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h5>Loading...</h5>
+      </React.Fragment>
+    );
+  }
+}
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    mongoURI:
+      "mongodb+srv://film:123@film.7ir5x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
